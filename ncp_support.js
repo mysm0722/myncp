9b1d171debccd038be365810e972824e26b5779c
root <mysm0722@gmail.com>
2018-01-30 21:13:44 +0900

    init: src update

---
+++ ncp_support.js
@@ -0,0 +1,119 @@
+
+const https = require('https');
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const reply = require('./reply');
+
+var app = express();
+
+// set SSL
+var https_options = {
+    ca: fs.readFileSync('/etc/letsencrypt/live/bot.mycloud1.ga/chain.pem', 'utf8'),
+    key: fs.readFileSync('/etc/letsencrypt/live/bot.mycloud1.ga/privkey.pem', 'utf8'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/bot.mycloud1.ga/fullchain.pem', 'utf8')
+};
+
+app.use(bodyParser.json());
+
+app.get('/webhook', function (reqeust, response) {
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.end('<h1>Changhwan - mysm0722@naver.com<h1>');
+});
+
+var helpText =
+    "[ NCP Bot Help ] \n" +
+    "@ncp : call hello ncp \n" +
+    "@guide || @g : view NCP User Guide \n" +
+    "@portal || @p : view NCP Portal Site \n" +
+    "@help || @h : view action list of momo \n" +
+    "[ NCP Bot Help ]";
+
+var helpArrayText = [
+    {
+        "type": "text",
+        "text": helpText
+    }
+];
+
+var guideUrlText =
+    "[ NCP User Guide ]\n" +
+    "NCP User Guide : http://docs.ncloud.com/ko/\n"; 
+
+var guideUrlArrayText = [
+    {
+        "type": "text",
+        "text": guideUrlText
+    }
+];
+
+var portalUrlText =
+    "[ NCP Portal Site ]\n" +
+    "NCP Portal Site : http://www.ncloud.com/\n"; 
+
+var portalUrlArrayText = [
+    {
+        "type": "text",
+        "text": portalUrlText
+    }
+];
+
+var errorText =
+    "Hi~ This NCP LINE Bot.\nThis Command is not supported.\n";
+
+var errorArrayText = [
+    {
+        "type": "text",
+        "text": errorText 
+    }
+];
+
+app.post('/webhook', function (request, response) {
+
+    var eventObj = request.body.events[0];
+    var source = eventObj.source;
+    var message = eventObj.message;
+
+    var CHANNEL_ACCESS_TOKEN = 'Mqw0wduN0NeUQElYF2jmwGpWoa0Llpki52yOfyTCVbCmD2zQZXXi2Lqyo0jWrzXjha2GYtkLqmZouslQU23XjazFuuWo6DRbJkhHZga1Aqdnfl0sEguwqmtMoIHAC+I44ecGCXjORMBW5gdGPW+LiQdB04t89/1O/w1cDnyilFU=';
+
+    // request log
+    console.log('======================', new Date() ,'======================');
+    console.log('[request]', request.body);
+    console.log('[request source] ', eventObj.source);
+    console.log('[request message]', eventObj.message);
+
+	console.log(/^@.+/g.test(message.text));	
+    
+    if(message.type = "text" && message.text.indexOf("@ncp") != -1){
+        reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, errorArrayText );
+    }
+    //else if(message.type = "text" && /^@.+/g.test(message.text)){
+	else {
+        var cmd = message.text.split('@')[1];
+        console.log('[command]', cmd);
+
+        if(typeof cmd !== "undefined" && cmd != ""){
+            if(cmd == "h" || cmd == "help"){
+                reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, helpArrayText);
+            }
+            else if(cmd == "guide" || cmd == "g"){
+                reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, guideUrlArrayText);
+            }
+            else if(cmd == "portal" || cmd == "p"){
+                reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, portalUrlArrayText);
+            }
+        } else {
+
+			console.log('reply tok : ' + eventObj.replyToken);
+			console.log('ch tok : ' + CHANNEL_ACCESS_TOKEN);
+
+			reply.send(CHANNEL_ACCESS_TOKEN, eventObj.replyToken, errorArrayText);
+		}
+    }
+    
+    response.sendStatus(200);
+});
+
+https.createServer(https_options, app).listen(443, function(){
+    console.log("@NCP Support Bot Server is Running");
+});
---
+++ reply.js
@@ -0,0 +1,40 @@
+
+const requestSender = require('request');
+
+module.exports.send = function (channelAccessToken, replyToken, messages) {
+
+
+	console.log('reply.channelAccessToken : ' + channelAccessToken);
+	console.log('reply.replyToken : ' + replyToken);
+	console.log('reply.messages : ' + messages);
+
+    var headers = {
+        'Content-type' : 'application/json',
+        'Authorization' : 'Bearer ' + channelAccessToken
+    };
+
+    var options = {
+        url: 'https://api.line.me/v2/bot/message/reply',
+        method: 'POST',
+        headers: headers,
+        json: {
+            replyToken : replyToken,
+            messages : messages
+        }
+    };
+
+    requestSender(options, function (error, response, body) {
+
+		console.log('req.Sender.options : ' + options);
+		console.log('req.Sender.body : ' + body);
+        console.log('response', response.statusCode);
+
+        if (!error && response.statusCode == 200) {
+            console.log(body)
+        }
+        else{
+            console.log('requestSender', error);
+        }
+    })
+
+};
